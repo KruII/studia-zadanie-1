b4b2ac17466846ce26a2d9a91c4ed54b253f9c88
KruII <KruII@users.noreply.github.com>
2023-11-16 18:01:47 +0100

    v0.02

--- example.py
+++ example.py
@@ -1,1 +1,24 @@
+import hashlib
+import os
+
+def hash_haslo(password):
+    # Tworzenie soli
+    salt = os.urandom(16)
+
+    # Hashowanie hasła z solą
+    hashed_haslo = hashlib.pbkdf2_hmac(
+        'sha256',  # Używany algorytm skrótu
+        password.encode('utf-8'),  # Konwersja hasła na bajty
+        salt,  # Sól
+        100000  # Liczba iteracji
+    )
+    return salt + hashed_password
+
 print("Hello, world!")
+
+login = input("Podaj login: ")
+haslo = input("Podaj haslo: ")
+
+
+hashed = hash_password(haslo)
+print(login, hashed)
